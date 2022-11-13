08883efed3353d4bf1f1e07dca20ecfd4ec1c524
Chancellor-art <66575368+Chancellor-art@users.noreply.github.com>
2022-11-13 21:36:20 +0200

    send message in time

--- index.js
+++ index.js
@@ -1,87 +1,90 @@
 const TELEGRAM_API = require('node-telegram-bot-api');
 const fs = require('fs');
 
-//Your telegram nick name
-const ADMIN = 'YOUR_NICKNAME_TELEGRAM';
-
-//Your json file
-const jsonFile = 'birthday.json';
-
 //Replace the value below with the Telegram token you receive from @BotFather
 const TOKEN = 'YOUR_TELEGRAM_TOKEN';
+
+//Your telegram nick name
+const ADMIN = 'YOUR_NICKNAME';
+
+//Your json file with birthday
+const birhtdayJsonFile = 'birthday.json';
+
+//Time to send message in hours and minutes
+const TIME = 8;
+const MINUTES = 0;
 
 //Create a bot that uses 'polling' to fetch new updates
 const bot = new TELEGRAM_API(TOKEN, {polling: true});
 
 let listBirthday = { usersDate: [] };
 
-//Listen new user and send message
-bot.on('new_chat_members', msg => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'ТУТ ПРИВІТАННЯ');
-});
-
-//Listen left user and send message
-bot.on('left_chat_member', msg => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Пака');
-});
-
-//Command start, send welcome message
-bot.onText(/\/start/, msg => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привіт я Толік(бот) і моя задача вітати нових людей в чаті та вітати з днем народження.');
-});
-
 //Buttons for settings
 const buttonsOptions = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
           [{ text: 'Переглянути базу даних', callback_data: '0' }],
-          [{ text: 'Добавити користувача', callback_data: '1' }],
+          [{ text: 'Додати користувача', callback_data: '1' }],
           [{ text: 'Видалити користувача', callback_data: '2' }]
         ]
     })
 };
 
-//Command settings, send buttons for settings
-bot.onText(/\/settings/, msg => {
-    const chatId = msg.chat.id;
-    if (msg.from.username === ADMIN) {
-        bot.sendMessage(chatId, 'Ви авторизовані як супер користувач', buttonsOptions);  
-    } else {
-        bot.sendMessage(chatId, 'Ви не маєте доступу до налаштування');
-        bot.sendMessage(chatId, 'Не сумуйте :)');
-    }
-});
-
 //Add new user
 const addNewUser = (name, time) => {
-    fs.readFile(jsonFile, 'utf8', function readFileCallback(err, data){
+    fs.readFile(birhtdayJsonFile, 'utf8', function readFileCallback(err, data){
         if (err) {
             console.log(err);
         } else {
             listBirthday = JSON.parse(data);
             listBirthday.usersDate.push({name: `${name}`, date: `${time}`});
-            fs.writeFileSync(jsonFile, JSON.stringify(listBirthday));
+            fs.writeFileSync(birhtdayJsonFile, JSON.stringify(listBirthday));
             console.log(`Add new user successfully! ${name} ${time}`);
         }
     });
-}
+};
 
 //Delete user
 const deleteUser = (userPosition) => {
-    fs.readFile(jsonFile, 'utf8', function readFileCallback(err, data){
+    fs.readFile(birhtdayJsonFile, 'utf8', function readFileCallback(err, data){
         if (err){
             console.log(err);
         } else {
             listBirthday = JSON.parse(data);
             delete listBirthday.usersDate[userPosition];
             listBirthday.usersDate.splice(userPosition, 1);
-            fs.writeFileSync(jsonFile, JSON.stringify(listBirthday));
+            fs.writeFileSync(birhtdayJsonFile, JSON.stringify(listBirthday));
         }
     });
-}
+};
+
+//Listen new user and send message
+bot.on('new_chat_members', msg => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привіт! Вітаємо у нашому дружньому колективі');
+});
+
+//Listen left user and send message
+bot.on('left_chat_member', msg => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Оуу, як сумно(');
+});
+
+//Command start, send welcome message
+bot.onText(/\/start/, msg => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привіт! я Толік-харізмат, бот, що нагадуватиме тобі про дні народження. А то ви - людішкі, постійно забуваєте)');
+});
+
+//Command settings, send buttons for settings
+bot.onText(/\/settings/, msg => {
+    const chatId = msg.chat.id;
+    if (msg.from.username == ADMIN) {
+        bot.sendMessage(chatId, 'Ви авторизовані як супер користувач', buttonsOptions);  
+    } else {
+        bot.sendMessage(chatId, 'Ви не маєте доступу до налаштувань, не сумуйте :)');
+    }
+});
 
 //Button functionality
 bot.on('callback_query', msg => {
@@ -90,15 +93,15 @@ bot.on('callback_query', msg => {
 
     //Pars JSOM file and send user name and date
     if (date === '0') {
-        fs.readFile(jsonFile, 'utf8', function readFileCallback(err, data){
+        fs.readFile(birhtdayJsonFile, 'utf8', function readFileCallback(err, data){
             if (err){
                 console.log(err);
             } else {
                 listBirthday = JSON.parse(data);
                 if (listBirthday.usersDate.length == 0) {
-                    bot.sendMessage(chatId, 'База даних пуста, добавте користувачів для її перегляду');
+                    bot.sendMessage(chatId, 'База даних пуста, додайте користувача для її перегляду');
                 } else {
-                    bot.sendMessage(chatId, 'Базі даних такі користувачі: ');
+                    bot.sendMessage(chatId, 'В базі даних такі користувачі: ');
                     for (let i = 0; i < listBirthday.usersDate.length; i++) {
                         let name = listBirthday.usersDate[i].name;
                         let date = listBirthday.usersDate[i].date;
@@ -114,7 +117,7 @@ bot.on('callback_query', msg => {
     else if (date === '1') {
         let name = null;
         let date = null;
-        bot.sendMessage(chatId, 'Напишіть фамалія та імя для додавання користувача. Приклад: (Пупкін Вася)', {
+        bot.sendMessage(chatId, 'Напишіть призвіще та імя для додавання користувача. Приклад: (Пупкін Вася)', {
             reply_markup: {
                 force_reply: true
             }
@@ -122,7 +125,7 @@ bot.on('callback_query', msg => {
             bot.onReplyToMessage(addApiId.chat.id, addApiId.message_id, async msg => {
                 name = msg.text;
 
-                bot.sendMessage(chatId, 'Напишіть дату народження. Приклад:(31.12.2022)', {
+                bot.sendMessage(chatId, 'Напишіть дату народження без року. Приклад:(31.12)', {
                     reply_markup: {
                         force_reply: true
                     }
@@ -130,7 +133,7 @@ bot.on('callback_query', msg => {
                         bot.onReplyToMessage(addApiId.chat.id, addApiId.message_id,async msg => {
                         date = msg.text;
                         addNewUser(name, date);
-                        bot.sendMessage(chatId, 'Новий користувач успішно добавлений');
+                        bot.sendMessage(chatId, 'Новий користувач успішно доданий');
                     });
                 });
             });
@@ -139,7 +142,7 @@ bot.on('callback_query', msg => {
 
     //Left users with json
     else if (date === '2') {
-        bot.sendMessage(chatId, 'Напишіть фамалія та імя для видалення користувача. Приклад: (Пупкін Вася)', {
+        bot.sendMessage(chatId, 'Напишіть призвіще та імя для видалення користувача. Приклад: (Пупкін Вася)', {
             reply_markup: {
                 force_reply: true
             }
@@ -147,7 +150,7 @@ bot.on('callback_query', msg => {
                 bot.onReplyToMessage(addApiId.chat.id, addApiId.message_id,async msg => {
                 let nameDelete = msg.text;
 
-                fs.readFile(jsonFile, 'utf8', function readFileCallback(err, data){
+                fs.readFile(birhtdayJsonFile, 'utf8', function readFileCallback(err, data){
                     if (err){
                         console.log(err);
                     } else {
@@ -158,7 +161,7 @@ bot.on('callback_query', msg => {
                                 deleteUser(i);
                                 let name = listBirthday.usersDate[i].name;
                                 let date = listBirthday.usersDate[i].date;
-                                console.log(chatId, `Delete user ${name} ${date}`);
+                                console.log(chatId, `Delete user. ${name} ${date}`);
                                 break;
                             }
                         }
@@ -167,8 +170,31 @@ bot.on('callback_query', msg => {
                 bot.sendMessage(chatId, 'Користувач був видалений');
             });
         });
-    } 
-    else {
+    } else {
         bot.sendMessage(chatId, 'Кнопка не робоча');
     }
 });
+
+function happyBirthday() {
+    let test = 'YOUR_ID_CHAT';
+    let nowDate = new Date();
+    let currentDate = `${nowDate.getDate()}.${nowDate.getMonth() + 1}`;
+
+    fs.readFile(birhtdayJsonFile, 'utf8', function readFileCallback(error, data){
+        if (error){
+            console.log(error);
+        } else {
+            listBirthday = JSON.parse(data);
+            for (let i = 0; i < listBirthday.usersDate.length; i++) {
+                if (currentDate == listBirthday.usersDate[i].date) {
+                    if (TIME == nowDate.getHours() & MINUTES == nowDate.getMinutes()) {
+                        bot.sendMessage(test, `Сьогодні день народження ${listBirthday.usersDate[i].name} - привітай та купи смаколик!`);
+                        console.log(listBirthday.usersDate[i].name);
+                    }
+                }
+            }
+        }
+    });
+};
+  
+setInterval(happyBirthday, 30000);
